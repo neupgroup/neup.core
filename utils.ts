9eb9da12d99447b84f48ce559e430370c4929b6c
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2026-07-28 09:56:52 +0545

    Added save before installing command help comment.

--- utils.ts
+++ utils.ts
@@ -1,8 +1,8 @@
 // General utility helpers used across the UI layer.
 
-// Getting error importing, run "npm install clsx"
+// Getting error importing, run "npm install clsx --save"
 import { clsx, type ClassValue } from "clsx"
-// Getting error importing, run "npm install tailwind-merge"
+// Getting error importing, run "npm install tailwind-merge --save"
 import { twMerge } from "tailwind-merge"
 
 // Merges Tailwind CSS class names, resolving conflicts via tailwind-merge
